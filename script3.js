17468cae73429818b2406e40e89c1a5388880931
moemen saadeh <99843903+Moemen12@users.noreply.github.com>
2022-05-31 00:08:15 +0300

    Create To-Do List

---
+++ script3.js
@@ -0,0 +1,144 @@
+/*
+  Students Tasks:
+  [1] Use Sweet Alert If Input Is Empty
+  [2] Check If Task Is Exist
+  [3] Create Delete All Tasks Button
+  [4] Create Finish All Tasks Button
+  [5] Add To Tasks To The Local Storage
+*/
+
+// Setting Up Variables
+let theInput = document.querySelector(".add-task input");
+let theAddButton = document.querySelector(".add-task .plus");
+let tasksContainer = document.querySelector(".tasks-content");
+let tasksCount = document.querySelector(".tasks-count span");
+let tasksCompleted = document.querySelector(".tasks-completed span");
+
+// Focus On Input Field
+window.onload = function () {
+  theInput.focus();
+};
+
+// Adding The Task
+theAddButton.onclick = function () {
+
+  // If Input is Empty
+  if (theInput.value === '') {
+
+    console.log("No Value");
+
+  } else {
+
+    let noTasksMsg = document.querySelector(".no-tasks-message");
+
+    // Check If Span With No Tasks Message Is Exist
+    if (document.body.contains(document.querySelector(".no-tasks-message"))) {
+
+      // Remove No Tasks Message
+      noTasksMsg.remove();
+
+    }
+
+    // Create Main Span Element
+    let mainSpan = document.createElement("span");
+
+    // Create Delete Button
+    let deleteElement = document.createElement("span");
+
+    // Create The Main Span Text
+    let text = document.createTextNode(theInput.value);
+
+    // Create The Delete Button Text
+    let deleteText = document.createTextNode("Delete");
+
+    // Add Text To Main Span
+    mainSpan.appendChild(text);
+
+    // Add Class To Main Span
+    mainSpan.className = 'task-box';
+
+    // Add Text To Delete Button
+    deleteElement.appendChild(deleteText);
+
+    // Add Class To Delete Button
+    deleteElement.className = 'delete';
+
+    // Add Delete Button To Main Span
+    mainSpan.appendChild(deleteElement);
+
+    // Add The Task To The Container
+    tasksContainer.appendChild(mainSpan);
+
+    // Empty The Input
+    theInput.value = '';
+
+    // Focus On Field
+    theInput.focus();
+
+    // Calculate Tasks
+    calculateTasks();
+
+  }
+
+};
+
+document.addEventListener('click', function (e) {
+
+  // Delete Task
+  if (e.target.className == 'delete') {
+
+    // Remove Current Task
+    e.target.parentNode.remove();
+
+    // Check Number Of Tasks Inside The Container
+    if (tasksContainer.childElementCount == 0) {
+
+      createNoTasks();
+
+    }
+
+  }
+
+  // Finish Task
+  if (e.target.classList.contains('task-box')) {
+
+    // Toggle Class 'finished'
+    e.target.classList.toggle("finished");
+
+  }
+
+  // Calculate Tasks
+  calculateTasks();
+
+});
+
+// Function To Create No Tasks Message
+function createNoTasks() {
+
+  // Create Message Span Element
+  let msgSpan = document.createElement("span");
+
+  // Create The Text Message
+  let msgText = document.createTextNode("No Tasks To Show");
+
+  // Add Text To Message Span Element
+  msgSpan.appendChild(msgText);
+
+  // Add Class To Message Span
+  msgSpan.className = 'no-tasks-message';
+
+  // Append The Message Span Element To The Task Container
+  tasksContainer.appendChild(msgSpan);
+
+}
+
+// Function To Calculate Tasks
+function calculateTasks() {
+
+  // Calculate All Tasks
+  tasksCount.innerHTML = document.querySelectorAll('.tasks-content .task-box').length;
+
+  // Calculate Completed Tasks
+  tasksCompleted.innerHTML = document.querySelectorAll('.tasks-content .finished').length;
+
+}
